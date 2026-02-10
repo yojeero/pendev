d96bf05c3efb164711d25cab04e66b21cda4bf4d
Yojee <yojeero@gmail.com>
2026-02-10 23:24:22 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 
-/* active menu button color */
+// active menu button color
 $(function(){$('a').each(function(){if($(this).prop('href')==window.location.href){$(this).addClass('active');$(this).parents('li').addClass('active');}});});
 
-/* hamburger animation*/
+// hamburger animation
 var menu=document.querySelector(".navicon");menu.addEventListener("click",function(){this.classList.toggle('active');});
 
-/* hamburger open nav */
+// hamburger open nav
 jQuery(function(){$('.navicon').on('click',function(){$(this).toggleClass('act');$('.menu').toggleClass('open');});$('.nav li a').on('click',function(){$('.menu').removeClass('open');$('.navicon').removeClass('open');});});
 
-/* animate scroll */
+// animate scroll
 jQuery(function(){$('.link_to').click(function(){var scroll_el=$(this).attr('href');if($(scroll_el).length!=0){$('html, body').animate({scrollTop:$(scroll_el).offset().top},1000);} return false;});});
 
-/* scroll up */
+// scroll up 
 $(function(){$(window).scroll(function(){var a=$(window).scrollTop();1200<a&&$(".up").fadeIn("slow");1200>a&&$(".up").fadeOut("slow")});$(".up").click(function(){$("html, body").animate({scrollTop:"0"},500)})});
+
+// accordion price
+var i,acc=document.getElementsByClassName("accordion");for(i=0;i<acc.length;i++)acc[i].addEventListener("click",function(){this.classList.toggle("active");var e=this.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"});
